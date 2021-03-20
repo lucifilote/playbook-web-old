1796201cf4ec74b927a4d734ebef11408d1d7539
https://git.cgm.ag/petricalucian.filote <petricalucian.filote@cgm.com>
2021-03-20 22:44:54 +0100

    removed redirect - we are handle it internally

--- components/AuthCard.tsx
+++ components/AuthCard.tsx
@@ -15,8 +15,6 @@ const useStyles = makeStyles({
 const uiConfig = {
     // Popup signin flow rather than redirect flow.
     signInFlow: 'popup',
-    // Redirect to /signedIn after sign in is successful. Alternatively you can provide a callbacks.signInSuccess function.
-    signInSuccessUrl: '/',
     // We will display Google and Facebook as auth providers.
     signInOptions: [
         firebase.auth.GoogleAuthProvider.PROVIDER_ID,
