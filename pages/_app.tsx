544d5346f980cf2bbbdc86a7c73d6b98025eeba7
https://git.cgm.ag/petricalucian.filote <petricalucian.filote@cgm.com>
2021-03-20 22:45:24 +0100

    integrated authprovider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,12 @@
 import { ThemeProvider } from '@material-ui/styles';
+import React from 'react';
 import "tailwindcss/tailwind.css";
+import AuthProvider from '../components/AuthProvider';
 import theme from '../config/theme';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
-  return <ThemeProvider theme={theme}><Component {...pageProps} /></ThemeProvider>
+  return <ThemeProvider theme={theme}> <AuthProvider><Component {...pageProps} /></AuthProvider></ThemeProvider>
 }
 
 export default MyApp
