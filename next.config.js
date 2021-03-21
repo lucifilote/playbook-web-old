11b0d2efbb1a108d7fff87cd4206d6a3d29db125
https://git.cgm.ag/petricalucian.filote <petricalucian.filote@cgm.com>
2021-03-21 16:50:55 +0100

    added firebase admin env props

--- next.config.js
+++ next.config.js
@@ -9,5 +9,14 @@ module.exports = {
         STORAGE_BUCKET: process.env.STORAGE_BUCKET,
         MESSAGING_SENDER_ID: process.env.MESSAGING_SENDER_ID,
         APP_ID: process.env.APP_ID,
+        TYPE: process.env.TYPE,
+        PRIVATE_KEY_ID: process.env.PRIVATE_KEY_ID,
+        PRIVATE_KEY: process.env.PRIVATE_KEY,
+        CLIENT_EMAIL: process.env.CLIENT_EMAIL,
+        CLIENT_ID: process.env.CLIENT_ID,
+        AUTH_URI: process.env.AUTH_URI,
+        TOKEN_URI: process.env.TOKEN_URI,
+        AUTH_PROVIDER_X509_CERT_URL: process.env.AUTH_PROVIDER_X509_CERT_URL,
+        CLIENT_X509_CERT_URL: process.env.CLIENT_EMAIL
     },
 }
